d2e4ab5f1d537ab8062505f61f95985806221ec4
artem <o971ooo811@gmail.com>
2016-04-16 14:48:30 +0300

    first commit

---
+++ src/main/java/uits/jv1604/algorithms/Lines.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uits.jv1604.algorithms;
+
+/**
+ *
+ * @author Артём
+ */
+public class Lines {
+    
+    public static void main(String[] args) {
+    
+        String s = "это одна строа кода, "
+                + "это вторая строка кода, "
+                + "это третья строка кода.";
+     
+        System.out.println(s);
+        System.out.print("это были три сроки в коде, дающие одну строку в консоли");
+        
+    }
+}
